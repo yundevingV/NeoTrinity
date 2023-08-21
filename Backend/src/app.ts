38ad821d1213ab3c52486cb7d45051411e9e9cf1
DEMl4N <delfirmamentos@gmail.com>
2023-08-22 01:12:38 +0900

    Feat: 리액트 올리기

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -1,10 +1,20 @@
 // import express, { Application, Request, Response } from 'express'
 import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
+import path from 'path'
 
 const app: Application = express()
 
+app.use(express.json())
+app.use(cors())
+app.use(express.static(path.join(__dirname, '../Frontend/build')))
+
 app.get('/', (req: Request, res: Response) => {
-    res.send('Hello World')
+    res.sendFile(path.join(__dirname, '../Frontend/build/index.html'))
 })
+
+app.get('*', function (req: Request, res: Response) {
+    res.sendFile(path.join(__dirname, '../Frontend/build/index.html'));
+  });
 
 export { app }
