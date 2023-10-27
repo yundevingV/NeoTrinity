898dddeab94a70999b803c947cff05dc3ff6c409
HoChanny <qboooodp@naver.com>
2023-10-28 01:44:40 +0900

    Feat: Sidebar, Mainpage 구현

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,33 +1,14 @@
 import React from 'react';
-
-import { Route, Routes } from 'react-router-dom';
+import logo from './logo.svg';
 import './App.css';
-import SignIn from './pages/Signin'
-import SignUp from './pages/Signup'
-import Main from './pages/MainPage'
-import Group from './pages/Group/Group'
-import GroupNotice from './pages/Group/GroupNotice'
-import GroupNoticeDetail from './pages/Group/GroupNoticeDetail';
-import GroupVote from './pages/Group/GroupVote';
-import GroupVoteDetail from './pages/Group/GroupVoteDetail';
+import SideBar from './components/SideBar/SideBar';
+import MainPage from './pages/MainPage';
 
 function App() {
   return (
-    <div className="App">
-      <Routes>
-        <Route path='/' element = {<SignIn />} />
-        <Route path='/signup' element = {<SignUp />} />
-        <Route path='/main' element = {<Main />} />
-
-        {/* group */}
-        <Route path='/group' element = {<Group />} />
-        <Route path='/group/notice' element = {<GroupNotice />} />
-        <Route path='/group/notice/detail/*' element = {<GroupNoticeDetail />} />
-        <Route path='/group/vote/' element = {<GroupVote />} />
-        <Route path='/group/vote/detail/*' element = {<GroupVoteDetail />} />
-
-      </Routes>
-    </div>
+    <>
+    <SideBar content={<MainPage/>}/>
+    </>
   );
 }
 
